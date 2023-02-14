2b6ef413385f2326c12449c87575bbbfbb4ec3f6
YuriySamusew <6177385@gmail.com>
2023-02-14 20:53:31 +0200

    Searchbar rewryghting to Hooks

--- src/components/Searchbar/Searchbar.jsx
+++ src/components/Searchbar/Searchbar.jsx
@@ -1,7 +1,5 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import PropTypes from 'prop-types';
-// import { Formik } from 'formik';
-// import * as yup from 'yup';
 
 import {
   SearchbarStyled,
@@ -10,15 +8,52 @@ import {
   BtnSearchForm,
   SearchFormBtnLabel,
 } from 'components/Searchbar/Searchbar.styled';
+
 import { FiSearch } from 'react-icons/fi';
 
-/*
-const nameRegExp = /^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$/;
-const schema = yup.object().shape({
-  name: yup.string().matches(nameRegExp, 'Name is not valid'),
-});
-*/
+const Searchbar = ({ onSubmit }) => {
+  const [search, setSearch] = useState('');
 
+  const handleChange = e => {
+    setSearch(e.currentTarget.value.toLowerCase());
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    onSubmit({ search });
+    setSearch('');
+  };
+
+  return (
+    <SearchbarStyled>
+      <SearchForm onSubmit={handleSubmit}>
+        <BtnSearchForm type="submit">
+          <FiSearch />
+          <SearchFormBtnLabel>Search</SearchFormBtnLabel>
+        </BtnSearchForm>
+
+        <SearchFormInput
+          value={search}
+          onChange={handleChange}
+          name="search"
+          className="input"
+          type="text"
+          autoComplete="off"
+          autoFocus
+          placeholder="Search images and photos"
+        />
+      </SearchForm>
+    </SearchbarStyled>
+  );
+};
+
+export default Searchbar;
+
+Searchbar.propTypes = {
+  onSubmit: PropTypes.func.isRequired,
+};
+
+/*
 class Searchbar extends Component {
   state = { search: '' };
 
@@ -64,39 +99,4 @@ class Searchbar extends Component {
     );
   }
 }
-
-export default Searchbar;
-
-Searchbar.propTypes = {
-  onSubmit: PropTypes.func.isRequired,
-};
-
-/*
-
-  return (
-<SearchField>
-  <Formik
-    initialValues={initialValues}
-    validationSchema={schema}
-    onSubmit={handleSubmit}
-  >
-    <SearchinForm autoComplete="off">
-      <BtnSearchForm type="submit">
-        <span>Search</span>
-      </BtnSearchForm>
-
-      <InputSearchForm
-        type="text"
-        name="name"
-        autoComplete="off"
-        autoFocus
-        placeholder="Search images and photos"
-      ></InputSearchForm>
-      <Error name="name" component="div" />
-    </SearchinForm>
-  </Formik>
-</SearchField>
-  );
-};
-
 */
